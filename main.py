a0ee5d6d291c44703cc0da5dd9ea9b2177c3ec02
Mairon Santana <mairon.santana@ifce.edu.br>
2025-08-25 14:01:46 +0000

    mudancas

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="API do Mairon")
+
+
+@app.get("/")
+def helloWorld():
+    return {"message": "Hello World!"}
+
+@app.get("/aluno")
+def get_aluno():
+    return {"insira na rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return{"o nome do aluno é": nome_aluno}
